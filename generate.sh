3891a4a409d06cf37c25c7bb60db84973dd0063c
wizjany <wizjany@gmail.com>
2026-09-16 00:44:59 -0400

    Update script/deps for STDB 2.x.

--- generate.sh
+++ generate.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
+set -e
+
+repo_root=$(pwd)
+
+hostname=${BITCRAFT_SPACETIME_HOST:-bitcraft-early-access.spacetimedb.com}
+global_mod=${BITCRAFT_GLOBAL_MODULE:-bitcraft-live-global}
+region_mod=${BITCRAFT_REGION_MODULE:-bitcraft-live-2}
 
 test -d src/global && rm -r src/global
 test -d src/region && rm -r src/region
 
-# global bindings
-curl https://bitcraft-early-access.spacetimedb.com/v1/database/bitcraft-live-global/schema?version=9 | jq '{"V9": .}' > schema.json
-spacetime generate --module-def schema.json --lang rs --out-dir src/global
-rm schema.json
+# spacetime 2.x's `generate` hides --module-def from --help and only reaches
+# it once an unrelated "does a buildable module exist" check passes. A dummy
+# spacetimedb/ directory in the generate cwd satisfies that check without
+# spacetime ever looking inside it.
+scratch_dir=$(mktemp -d)
+mkdir -p "$scratch_dir/spacetimedb"
 
-# region bindings
-curl https://bitcraft-early-access.spacetimedb.com/v1/database/bitcraft-live-12/schema?version=9 | jq '{"V9": .}' > schema.json
-spacetime generate --module-def schema.json --lang rs --out-dir src/region
-rm schema.json
+generate() {
+  local db=$1 out=$2
+  curl --fail "https://${hostname}/v1/database/${db}/schema?version=10" | jq '{"V10": .}' > "$repo_root/schema.json"
+  # don't --include-private here, not that it makes a huge difference. we drop a few unnecessary tables, but reducers
+  # are pretty much all public and sender-gated instead.
+  (cd "$scratch_dir" && spacetime generate -y --module-def "$repo_root/schema.json" --lang rs --out-dir "$repo_root/$out")
+  rm "$repo_root/schema.json"
+}
 
-# strip message for version
-# ST_VER='1\.4\.0'
-# ST_REV=''
-# find src -name "*.rs" -type f -exec \
-#   perl -i -0pe "s|\n\n// This was generated using spacetimedb cli version $ST_VER \(commit $ST_REV\)\.||g" {} +
+generate ${global_mod} src/global
+
+generate ${region_mod} src/region
+
+rm -rf "$scratch_dir"
 
 # patch all DbUpdate fields to be pub
 perl -i -pe '/pub struct DbUpdate \{/ .. /^\}/ and s/^(\s+)(\w+:[^:])/$1pub $2/' src/global/mod.rs src/region/mod.rs
